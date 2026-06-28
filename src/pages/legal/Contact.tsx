fc359cb65daca0e76839627ae46bd9a6252a7b26
Aasimmuneer01 <aasimmuneer349@gmail.com>
2026-06-28 22:35:35 +0530

    Update Contact.tsx

--- src/pages/legal/Contact.tsx
+++ src/pages/legal/Contact.tsx
@@ -3,8 +3,9 @@ import LegalLayout from '../../components/LegalLayout';
 export default function Contact() {
   return (
     <LegalLayout title="Contact Us">
-      <p>Email: contact@eduplatform.com</p>
-      <p>WhatsApp: +1234567890</p>
+      <p>Email: aasimmuneer349@gmail.com</p>
+      <p>WhatsApp: Unavailable</p>
+      <p>Instagram: aasimmuneer349</p>
       {/* Add form later */}
     </LegalLayout>
   );
